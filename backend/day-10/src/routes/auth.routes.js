2e2d1104a05efd3049de37d6472eab9e80376d0b
albatino7 <atuldemond1@gmail.com>
2026-05-12 18:54:12 +0530

    hashing password jwt and crpto

--- backend/day-10/src/routes/auth.routes.js
+++ backend/day-10/src/routes/auth.routes.js
@@ -2,37 +2,75 @@ require("dotenv").config();
 const express = require("express");
 const userModel = require("../model/users.model");
 const jwt = require("jsonwebtoken");
+const crypto = require("crypto");
 
 const authRoute = express.Router();
 
 authRoute.post("/register", async (req, res) => {
   const { name, email, password } = req.body;
 
-  const isUserAlreadyExist = await userModel.findOne({ email });
-  if (isUserAlreadyExist) {
+  const user = await userModel.findOne({ email });
+  if (user) {
     return res.status(409).json({
       message: "User Already Exist",
     });
   }
 
-  const createdUser = await userModel.create({
+  const hashPassword = await crypto
+    .createHash("md5")
+    .update(password)
+    .digest("hex");
+
+  const newUser = await userModel.create({
     name: name,
     email: email,
-    password: password,
+    password: hashPassword,
   });
 
   const token = jwt.sign(
     {
-      id: createdUser._id,
-      email: createdUser.email,
+      id: newUser._id,
+      email: newUser.email,
     },
     process.env.JWT_SECRET,
   );
+
   res.cookie("jwt_token", token);
 
   res.status(201).json({
     message: "user Created Sucessfully",
-    createdUser,
+    newUser,
+    token,
+  });
+});
+
+authRoute.post("/login", async (req, res) => {
+  const { email, password } = req.body;
+
+  const user = await userModel.findOne({ email });
+
+  if (!user) {
+    return res.status(409).json({
+      message: "User Email Is Exist",
+    });
+  }
+
+  const isPasswordMatched =
+    user.password === crypto.createHash("md5").update(password).digest("hex");
+
+  if (!isPasswordMatched) {
+    return res.status(409).json({
+      message: "User Password iS not Correct ",
+    });
+  }
+
+  const token = jwt.sign({ userid: user._id }, process.env.JWT_SECRET);
+
+  res.cookie("jwt_token", token);
+
+  res.status(200).json({
+    message: "User Login Sucessfully",
+    user,
     token,
   });
 });
